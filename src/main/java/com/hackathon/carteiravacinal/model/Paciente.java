3a79667466036f2e6ceb9bb2642b0e9b65cb4f90
Jorgeluisreis <ctt.jluis@gmail.com>
2025-02-16 13:29:29 -0300

    feat: Adicionado o get/set do Id.

--- src/main/java/com/hackathon/carteiravacinal/model/Paciente.java
+++ src/main/java/com/hackathon/carteiravacinal/model/Paciente.java
@@ -8,12 +8,22 @@ public class Paciente {
         M, F
     }
 
+    private Long id;
     private String nome;
     private String cpf;
     private Sexo sexo;
     private LocalDate dataNascimento;
 
     // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
     }
