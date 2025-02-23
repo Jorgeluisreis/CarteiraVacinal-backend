7e1ed1de98658428b517fb1d645ab5e2d8adc351
Jorgeluisreis <ctt.jluis@gmail.com>
2025-02-23 17:44:44 -0300

    feat: Adicionado o Content-Type no CorsConfig.

--- src/main/java/com/hackathon/carteiravacinal/config/CorsConfig.java
+++ src/main/java/com/hackathon/carteiravacinal/config/CorsConfig.java
@@ -9,6 +9,7 @@ public class CorsConfig {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+            res.header("Content-Type", "application/json");
         });
 
         Spark.options("/*", (req, res) -> {
