58f91a96e417bd9e6b4ee6964f767ed7f2062e49
Jorgeluisreis <ctt.jluis@gmail.com>
2025-02-24 11:14:14 -0300

    raw: Alteração no CORS.

--- src/main/java/com/hackathon/carteiravacinal/config/CorsConfig.java
+++ src/main/java/com/hackathon/carteiravacinal/config/CorsConfig.java
@@ -5,22 +5,15 @@ import spark.Spark;
 public class CorsConfig {
 
     public static void configurarCORS() {
-        Spark.after((req, res) -> {
+        Spark.before((req, res) -> {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-            res.header("Content-Type", "application/json");
+            res.type("application/json");
         });
 
         Spark.options("/*", (req, res) -> {
-            String accessControlRequestHeaders = req.headers("Access-Control-Request-Headers");
-            if (accessControlRequestHeaders != null) {
-                res.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
-            }
-            String accessControlRequestMethod = req.headers("Access-Control-Request-Method");
-            if (accessControlRequestMethod != null) {
-                res.header("Access-Control-Allow-Methods", accessControlRequestMethod);
-            }
+            res.status(200);
             return "OK";
         });
     }
