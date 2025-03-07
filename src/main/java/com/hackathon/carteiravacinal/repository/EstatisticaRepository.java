dba3d57a7398bb42391d7582528b760f7aaedf4c
Jorgeluisreis <ctt.jluis@gmail.com>
2025-03-07 13:45:19 -0300

    fix: Corrigido a lógica de calculo de idade para a próxima vacina.

--- src/main/java/com/hackathon/carteiravacinal/repository/EstatisticaRepository.java
+++ src/main/java/com/hackathon/carteiravacinal/repository/EstatisticaRepository.java
@@ -35,34 +35,35 @@ public class EstatisticaRepository {
 
     public int qtdeVacinasProximasPorPaciente(Paciente paciente) throws ApiException {
         String query = """
-                    SELECT COUNT(*) AS total FROM dose d
-                    INNER JOIN vacina v ON d.id_vacina = v.id
-                    WHERE d.idade_recomendada_aplicacao = ?
-                    AND (v.limite_aplicacao IS NULL OR d.idade_recomendada_aplicacao <= v.limite_aplicacao)
-                    AND d.id NOT IN (
-                        SELECT i.id_dose FROM imunizacoes i WHERE i.id_paciente = ?
-                    )
-                """;
-
+                SELECT COUNT(*) AS total 
+                FROM dose d
+                INNER JOIN vacina v ON d.id_vacina = v.id
+                WHERE d.idade_recomendada_aplicacao = ? 
+                AND (v.limite_aplicacao IS NULL OR d.idade_recomendada_aplicacao <= v.limite_aplicacao)
+                AND d.id NOT IN (
+                    SELECT i.id_dose FROM imunizacoes i WHERE i.id_paciente = ?
+                )
+            """;
+    
         try (Connection conn = DatabaseConfig.getConnection();
-                PreparedStatement stmt = conn.prepareStatement(query)) {
-
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+    
             LocalDate dataNascimento = paciente.getDataNascimento();
             int idadeEmMeses = (int) Period.between(dataNascimento, LocalDate.now()).toTotalMonths();
-
-            stmt.setInt(1, idadeEmMeses + 1);
+    
+            stmt.setInt(1, idadeEmMeses);
             stmt.setLong(2, paciente.getId());
-
+    
             ResultSet resultSet = stmt.executeQuery();
             if (resultSet.next()) {
                 return resultSet.getInt("total");
             }
             return 0;
         } catch (SQLException e) {
-            throw new ApiException("Erro ao acessar o banco de dados para contar vacinas próximas: " + e.getMessage(),
-                    e);
+            throw new ApiException("Erro ao acessar o banco de dados para contar vacinas próximas: " + e.getMessage(), e);
         }
     }
+    
 
     public int qtdeVacinasAtrasadasPorPaciente(Paciente paciente) throws ApiException {
         String query = """
